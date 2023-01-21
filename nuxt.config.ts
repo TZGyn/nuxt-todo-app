3555b898057f977e4af1b827d6b4b9226f3b9799
TZGyn <ca538901d@mozmail.com>
2023-01-22 00:39:33 +0800

    added pinia and nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
+	modules: [
+		'@nuxtjs/supabase',
+		'@nuxtjs/tailwindcss',
+		'nuxt-icon',
+		'@pinia/nuxt',
+	],
 	alias: {
 		'@logo': '/assets/logo',
+		'@assets': '/assets',
+		'@stores': '/stores',
 	},
 	tailwindcss: {
 		config: {
