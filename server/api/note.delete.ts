43c1978794566be862920bd053d619376cc0286b
TZGyn <ca538901d@mozmail.com>
2023-02-04 20:38:46 +0800

    added note delete

---
+++ server/api/note.delete.ts
@@ -0,0 +1,25 @@
+import { serverSupabaseUser, serverSupabaseClient } from '#supabase/server';
+import { Database } from 'utils/database.types';
+
+export default defineEventHandler(async (event): Promise<DeleteResponse> => {
+	const user = await serverSupabaseUser(event);
+	const supabase = serverSupabaseClient<Database>(event);
+	const query = getQuery(event);
+
+	if (!user) {
+		throw createError({ statusCode: 401, message: 'Unauthorized' });
+	}
+
+	const { data: data, error } = await supabase
+		.from('notes')
+		.delete()
+		.eq('id', query.id)
+		.eq('user_id', user.id);
+
+	return {
+		status: 200,
+		data: data,
+		message: 'Note Deleted',
+		error: error,
+	};
+});
