b1defa285182c74648312c6d383bc973b6590998
TZGyn <ca538901d@mozmail.com>
2023-02-12 12:01:03 +0800

    update: header display

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
 	<div
-		class="bg-secondary flex h-fit w-full flex-row items-center justify-center p-4">
-		<div class="flex w-full max-w-3xl items-center justify-between">
+		class="bg-secondary flex h-fit w-full flex-row items-center justify-center p-2">
+		<div class="flex w-full max-w-3xl items-center justify-between gap-4">
 			<div
 				class="flex items-center gap-4 hover:cursor-pointer"
 				@click="navigateTo('/notes')">
@@ -10,12 +10,12 @@
 					name="fa6-solid:book-open" />
 				<p class="text-xl font-bold"> Notes </p>
 			</div>
-			<div
-				class="flex w-full justify-end"
-				@click="navigateTo('/profile')">
+			<div class="flex w-fit justify-end">
 				<Icon
 					class="bg-secondary rounded-full hover:cursor-pointer"
-					name="fa6-solid:circle-user" />
+					@click="navigateTo('/profile')"
+					name="fa6-solid:circle-user"
+					size="36" />
 			</div>
 		</div>
 	</div>
